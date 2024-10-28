68afbaf6e2d7969c7b765c20242038f9b8cc4985
Hvist-enota <oleksandr.mosiichuk@oa.edu.ua>
2024-10-28 18:34:54 +0200

    add: Application

--- BimbaApi/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ BimbaApi/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+232d501f00a577a261b6d1cec670247df24169d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
